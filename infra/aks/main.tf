d05ac376bc022dcb5b1d3a7946a6a8e7609cf18d
Daniyal <133042543+D1207-D@users.noreply.github.com>
2025-04-14 11:52:45 -0400

    Resolve merge conflict and update AKS configuration

--- infra/aks/main.tf
+++ infra/aks/main.tf
@@ -5,7 +5,7 @@ resource "azurerm_kubernetes_cluster" "aks" {
   dns_prefix          = "${var.project_name}-${var.environment}"
   kubernetes_version  = var.kubernetes_version
 
-  api_server_authorized_ip_ranges = ["0.0.0.0/0"] # TODO: Restrict this to your IP range
+  api_server_authorized_ip_ranges = ["0.0.0.0/0"]  # TODO: Restrict this to your IP range
 
   default_node_pool {
     name                = "default"
@@ -15,6 +15,16 @@ resource "azurerm_kubernetes_cluster" "aks" {
     enable_auto_scaling = true
     min_count           = 1
     max_count           = 3
+    os_disk_size_gb     = 50
+    os_sku              = "Ubuntu"
+    type                = "VirtualMachineScaleSets"
+    zones               = [1, 2, 3]
+
+    tags = {
+      Environment = var.environment
+      Project     = var.project_name
+      NodePool    = "default"
+    }
   }
 
   identity {
@@ -25,12 +35,48 @@ resource "azurerm_kubernetes_cluster" "aks" {
     network_plugin    = "azure"
     load_balancer_sku = "standard"
     network_policy    = "calico"
+    outbound_type     = "loadBalancer"
   }
 
   role_based_access_control_enabled = true
+  azure_policy_enabled             = true
+
+  oms_agent {
+    log_analytics_workspace_id = azurerm_log_analytics_workspace.aks.id
+  }
+
+  microsoft_defender {
+    enabled = true
+  }
 
   tags = {
     Environment = var.environment
     Project     = var.project_name
+  }
+}
+
+resource "azurerm_log_analytics_workspace" "aks" {
+  name                = "${var.project_name}-${var.environment}-aks-logs"
+  location            = var.location
+  resource_group_name = var.resource_group_name
+  sku                 = "PerGB2018"
+  retention_in_days   = 30
+
+  tags = {
+    Environment = var.environment
+    Project     = var.project_name
+  }
+}
+
+resource "azurerm_log_analytics_solution" "aks" {
+  solution_name         = "ContainerInsights"
+  location              = var.location
+  resource_group_name   = var.resource_group_name
+  workspace_resource_id = azurerm_log_analytics_workspace.aks.id
+  workspace_name        = azurerm_log_analytics_workspace.aks.name
+
+  plan {
+    publisher = "Microsoft"
+    product   = "OMSGallery/ContainerInsights"
   }
 }
