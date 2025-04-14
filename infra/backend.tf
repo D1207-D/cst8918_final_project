3a8008f7faa33b6270f9df8abc8d414e46cde24c
Daniyal <133042543+D1207-D@users.noreply.github.com>
2025-04-14 10:58:35 -0400

    Fix formatting in backend.tf

--- infra/backend.tf
+++ infra/backend.tf
@@ -4,6 +4,6 @@ terraform {
     storage_account_name = "tfstate5clng"
     container_name       = "tfstate"
     key                  = "terraform.tfstate"
-    use_azuread_auth    = true
+    use_azuread_auth     = true
   }
 }
