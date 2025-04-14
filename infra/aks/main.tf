2db4ff3610767f5ab5ae19cead5832e53b6e41a6
Daniyal <133042543+D1207-D@users.noreply.github.com>
2025-04-14 11:00:21 -0400

    Update AKS configuration with better settings and monitoring

--- infra/aks/main.tf
+++ infra/aks/main.tf
@@ -13,6 +13,16 @@ resource "azurerm_kubernetes_cluster" "aks" {
     enable_auto_scaling = true
     min_count           = 1
     max_count           = 3
+    os_disk_size_gb     = 50
+    os_sku              = "Ubuntu"
+    type                = "VirtualMachineScaleSets"
+    zones               = [1, 2, 3]
+
+    tags = {
+      Environment = var.environment
+      Project     = var.project_name
+      NodePool    = "default"
+    }
   }
 
   identity {
@@ -23,12 +33,48 @@ resource "azurerm_kubernetes_cluster" "aks" {
     network_plugin    = "azure"
     load_balancer_sku = "standard"
     network_policy    = "calico"
+    outbound_type     = "loadBalancer"
   }
 
   role_based_access_control_enabled = true
+  azure_policy_enabled             = true
+
+  oms_agent {
+    log_analytics_workspace_id = azurerm_log_analytics_workspace.aks.id
+  }
+
+  microsoft_defender {
+    enabled = true
+  }
 
   tags = {
     Environment = var.environment
     Project     = var.project_name
+  }
+}
+
+resource "azurerm_log_analytics_workspace" "aks" {
+  name                = "${var.project_name}-${var.environment}-aks-logs"
+  location            = var.location
+  resource_group_name = var.resource_group_name
+  sku                 = "PerGB2018"
+  retention_in_days   = 30
+
+  tags = {
+    Environment = var.environment
+    Project     = var.project_name
+  }
+}
+
+resource "azurerm_log_analytics_solution" "aks" {
+  solution_name         = "ContainerInsights"
+  location              = var.location
+  resource_group_name   = var.resource_group_name
+  workspace_resource_id = azurerm_log_analytics_workspace.aks.id
+  workspace_name        = azurerm_log_analytics_workspace.aks.name
+
+  plan {
+    publisher = "Microsoft"
+    product   = "OMSGallery/ContainerInsights"
   }
 }
--- infra/aks/variables.tf
+++ infra/aks/variables.tf
@@ -1,16 +1,19 @@
 variable "project_name" {
   description = "Name of the project"
   type        = string
+  default     = "cst8918-final"
 }
 
 variable "environment" {
   description = "Environment (dev, staging, prod)"
   type        = string
+  default     = "dev"
 }
 
 variable "location" {
   description = "Azure region"
   type        = string
+  default     = "canadacentral"
 }
 
 variable "resource_group_name" {
@@ -26,14 +29,17 @@ variable "subnet_id" {
 variable "kubernetes_version" {
   description = "Version of Kubernetes"
   type        = string
+  default     = "1.26.3"
 }
 
 variable "node_count" {
   description = "Number of AKS worker nodes in the default node pool"
   type        = number
+  default     = 1
 }
 
 variable "vm_size" {
   description = "Size of AKS worker nodes"
   type        = string
+  default     = "Standard_D2s_v3"
 }
